1b9e995cb678fc79ad1ce5bd9221432d20bc3305
Daniel Guerber <guerbi1990@gmx.ch>
2012-12-18 13:33:16 +0100

    init in if command

--- src/ch/fhnw/cpib/dgu/abstsyn/implementation/CmdIf.java
+++ src/ch/fhnw/cpib/dgu/abstsyn/implementation/CmdIf.java
@@ -2,7 +2,10 @@ package ch.fhnw.cpib.dgu.abstsyn.implementation;
 
 import java.sql.Types;
 
+import ch.fhnw.cpib.dgu.IMLCompiler;
 import ch.fhnw.cpib.dgu.abstsyn.IAbstSyn.ICmd;
+import ch.fhnw.cpib.dgu.context.Scope;
+import ch.fhnw.cpib.dgu.context.Store;
 
 public final class CmdIf implements ICmd {
 	private final IExpr expr;
@@ -45,8 +48,39 @@ public final class CmdIf implements ICmd {
                      expr.getLine());
         }
         
+        Scope parentScope = IMLCompiler.getScope();
+        Scope ifScope 
+            = new Scope(parentScope.getStoreTable().clone());
+        Scope elseScope 
+            = new Scope(parentScope.getStoreTable().clone());
+        
+        IMLCompiler.setScope(ifScope);
         ifCmd.check(canInit);
+        IMLCompiler.setScope(elseScope);
         elseCmd.check(canInit);
+        
+        for (Store store : parentScope.getStoreTable().getTable().values()) {
+            if (!store.isInitialized()) {
+                Store storeIf 
+                    = ifScope.getStoreTable().getStore(store.getIdent());
+                Store storeElse 
+                    = elseScope.getStoreTable().getStore(store.getIdent());
+                
+                if (storeIf.isInitialized() != storeElse.isInitialized()) {
+                    throw new ContextError(
+                            "Initialization must happen in both branches of an" 
+                                    + " IF command! Ident: " + store.getIdent(),
+                             expr.getLine());
+                }
+                
+                if (storeIf.isInitialized()) {
+                    store.initialize();
+                }
+            }
+        }
+        
+        IMLCompiler.setScope(parentScope);
+        
         repCmd.check(canInit);
     }
 }
